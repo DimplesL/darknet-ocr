8020e6232d9a0ff8c71932221b6d996979dd57a7
alex <cyy0523xc@gmail.com>
2020-01-13 18:19:55 +0800

    *

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ ocrPath = 'models/ocr/{}/ocr.weights'.format(ocrType)
 textPath = 'models/text/text.weights'
 darkRoot = 'darknet/libdarknet.so'   # darknet
 TEXT_LINE_SCORE = 0.7   # text line prob
+# scale, maxScale: 这两个参数会对识别效果影响比较大
+# 通常越大越好，该值越大则占用内存或者显存资源越大
+# 如果资源不够则会报错
 scale = 900             # 可动态修改 no care text.cfg height,width
 maxScale = 1800
 GPU = True              # gpu for darknet  or cpu for opencv.dnn
